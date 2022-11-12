17b99a127f98b8d9c4783a892df1a43f82a2ab81
Doven1Kanobe <ruslanosipov3524@yandex.ru>
2022-11-12 12:34:40 +0400

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    private String name;
+
+    public static void say() {
+        System.out.println("мяу!!!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
